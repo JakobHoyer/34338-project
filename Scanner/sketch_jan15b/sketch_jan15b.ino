89b66d56c0869f25ab0c0c150069f832d35e42a8
Søren Zepernick <soren.zepernick@gmail.com>
2025-01-15 13:47:17 +0100

    arbejde udført af Søren og Anel

--- Scanner/sketch_jan15b/sketch_jan15b.ino
+++ Scanner/sketch_jan15b/sketch_jan15b.ino
@@ -32,24 +32,54 @@
 
 #include <SPI.h>
 #include <MFRC522.h>
-//#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ESP8266HTTPClient.h>
 
 
 #define SS_PIN D8
 #define RST_PIN D2
  
 MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
-//const int rs = 18, en = 17, d4 = 16, d5 = 14, d6 = 13, d7 = 12;
-//LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 
 // Init array that will store new NUID 
 byte nuidPICC[4];
+char hexString[9];
 
 // Søren preset
 byte preset1[4] = {0xA2, 0x9F, 0x8A, 0x3F};
 
+int lcdColumns = 16;
+int lcdRows = 2;
+
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
+
+// Definerer id og password til netværksforbindelse som NodeMCU anvender
+const char* ssid = "Jakob - iPhone"; //Indsæt navnet på jeres netværk her
+const char* password = "kodekodeadad"; //Indsæt password her
+
+// Definerer information til mqtt serveren
+const char *mqtt_server = "maqiatto.com"; //navn på mqtt-server. Find navnet på cloudmqtt-hjemmesiden
+const int mqtt_port = 1883; // Definerer porten
+const char *mqtt_user = "s183668@student.dtu.dk"; // Definerer mqtt-brugeren
+const char *mqtt_pass = "kodekodeadad"; // Definerer koden til mqtt-brugeren
+const char *mqtt_topic = "s183668@student.dtu.dk/hub";
+//
+
+String payload; // Definerer variablen 'payload' i det globale scope (payload er navnet på besked-variablen)
 
 
+
+// Opretter en placeholder for callback-funktionen til brug senere. Den rigtige funktion ses længere nede.
+void callback(char* byteArraytopic, byte* byteArrayPayload, unsigned int length);
+
+// Opretter en klient der kan forbinde til en specifik internet IP adresse.
+WiFiClient espClient; // Initialiserer wifi bibloteket ESP8266Wifi, som er inkluderet under "nødvendige bibloteker"
+
+// Opretter forbindelse til mqtt klienten:
+PubSubClient client(mqtt_server, mqtt_port, callback, espClient); // Initialiserer bibloteket for at kunne modtage og sende beskeder til mqtt. Den henter fra definerede mqtt server og port. Den henter fra topic og beskeden payload
 
 
 
@@ -58,25 +88,54 @@ void setup() {
   Serial.begin(9600);
   SPI.begin(); // Init SPI bus
   rfid.PCD_Init(); // Init MFRC522 
-//lcd.begin(16, 2);
+  lcd.init();
+  lcd.backlight();
   // Print a message to the LCD.
-//  lcd.print("hello, world!");
   Serial.println(F("This code scan the MIFARE Classsic NUID."));
-  //Wire.begin();
-  //Serial.println("\nI2C Scanner");
+
+
+
+  setup_wifi(); // Kører WiFi loopet og forbinder herved.
+  client.setServer(mqtt_server, mqtt_port); // Forbinder til mqtt serveren (defineret længere oppe)
+  client.setCallback(callback); // Ingangsætter den definerede callback funktion hver gang der er en ny besked på den subscribede "cmd"- topic
+
 }
  
 void loop() {
 
+////// LOOP /////////
 
 
-  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+
+  // Hvis der opstår problemer med forbindelsen til mqtt broker oprettes forbindelse igen ved at køre client loop
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+   for (int i = 0; i < 4; i++) {
+    sprintf(&hexString[i * 2], "%02X", nuidPICC[i]);
+  }
+
+ // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
   if ( ! rfid.PICC_IsNewCardPresent())
     return;
 
   // Verify if the NUID has been readed
   if ( ! rfid.PICC_ReadCardSerial())
     return;
+
+  client.publish("s183668@student.dtu.dk/pub", hexString);
+  hexString[8]='\0';
+ 
+  delay(1000);
+
+//////// Loop slut ////////
+
+
+    lcd.setCursor(0, 0); // Set the LCD cursor position
+    lcd.print("Soerens card");
+
+
 
   Serial.print(F("PICC type: "));
   MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
@@ -119,6 +178,9 @@ void loop() {
 
 compareByteArrays(nuidPICC, preset1, 4);
 
+
+
+
 }
 
 
@@ -135,12 +197,12 @@ void compareByteArrays(byte *array1, byte *array2, size_t size) {
     // Print the result
     if (areIdentical) {
         Serial.println("The arrays are identical.");
-        //lcd.setCursor(0, 0); // Set the LCD cursor position
-        //lcd.print("Soerens card");
+        lcd.setCursor(0, 0); // Set the LCD cursor position
+        lcd.print("Soerens card");
     } else {
         Serial.println("invalid card");
-        //lcd.setCursor(0, 0); // Set the LCD cursor position
-        //lcd.print("Unknown card");
+        lcd.setCursor(0, 0); // Set the LCD cursor position
+        lcd.print("Unknown card");
     }
 }
 
@@ -151,16 +213,16 @@ void printHex(byte *buffer, byte bufferSize) {
   for (byte i = 0; i < bufferSize; i++) {
     Serial.print(buffer[i] < 0x10 ? " 0" : " ");
     Serial.print(buffer[i], HEX);
-    //lcd.setCursor(0, 1); // Set the LCD cursor position
-    //lcd.print("UID: ");
- // for (byte i = 0; i < 4; i++) {
+    lcd.setCursor(0, 1); // Set the LCD cursor position
+    lcd.print("UID: ");
+ for (byte i = 0; i < 4; i++) {
   
-  //    if (nuidPICC[i] < 0x10) lcd.print("0"); // Add a leading zero for single-digit hex values
-        //lcd.print(nuidPICC[i], HEX); // Print each byte in HEX
-      //if (i < 3) lcd.print(""); // Add a colon between bytes, except the last
+      if (nuidPICC[i] < 0x10) lcd.print("0"); // Add a leading zero for single-digit hex values
+        lcd.print(nuidPICC[i], HEX); // Print each byte in HEX
+      if (i < 3) lcd.print(""); // Add a colon between bytes, except the last
       
       
-      //}
+      }
   
  }
 
@@ -176,8 +238,119 @@ void printDec(byte *buffer, byte bufferSize) {
     Serial.print(' ');
     Serial.print(buffer[i], DEC);
     // ');
-    //lcd.print(buffer[i], DEC);
+   // lcd.print(buffer[i], DEC);
 
 
   }
 }
+
+// Definerer callback funktionen der modtager beskeder fra mqtt
+// OBS: den her funktion kører hver gang MCU'en modtager en besked via mqtt
+void callback(char* byteArraytopic, byte* byteArrayPayload, unsigned int length) {
+
+  // Konverterer indkomne besked (topic) til en string:
+  String topic;
+  topic = String(byteArraytopic);
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.println("] ");
+  // Konverterer den indkomne besked (payload) fra en array til en string:
+  // Topic == Temperaturmaaler, Topic == Kraftsensor
+  if (topic == mqtt_topic) { // OBS: der subscribes til et topic nede i reconnect-funktionen. I det her tilfælde er der subscribed til "Test". Man kan subscribe til alle topics ved at bruge "#"
+    payload = ""; // Nulstil payload variablen så forloopet ikke appender til en allerede eksisterende payload
+    for (int i = 0; i < length; i++) {
+      payload += (char)byteArrayPayload[i];
+      // For-loop: tag hvert tegn i hele længden af den inkomne besked, og konverter denne til en char. Append tegnene 1 efter 1:
+      // Eksempel:
+      // Besked = Study Abroad
+      // Length = 12
+      // Loop 1 = "S"
+      // Loop 2 = "St" osv.
+      // Loop (length) = "Study Abroad"
+
+      if (payload == "LED") {
+        // Definerer funktion som bruges til at styre temperaturmåling
+        
+        delay(1000);
+        client.publish(mqtt_topic, "LED er taendt");
+      }
+    }
+    Serial.println(payload);
+    //client.publish("mqtt", String(payload).c_str()); // Publish besked fra MCU til et valgt topic. Husk at subscribe til topic'et i NodeRed.
+  }
+
+}
+
+///////// CALLBACK SLUT /////////
+
+//
+//
+//
+//
+//
+//
+
+/////// OPSÆTNING AF WIFI-FORBINDELSE  ///////////
+
+
+// Opretter forbindelse til WiFi
+void setup_wifi() {
+  // Forbinder til et WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+// Opretter forbindelse til mqtt server. Dette gentages ved manglende forbindelse til WiFi, server osv.
+void reconnect() {
+  // Fortsætter til forbindelsen er oprettet
+  while (!client.connected()) {
+    Serial.print("Forsøger at oprette MQTT forbindelse...");
+
+    if (client.connect("GroupNamexMCU", mqtt_user, mqtt_pass)) { // Forbinder til klient med mqtt bruger og password
+      Serial.println("connected");
+      // Derudover subsribes til topic "Test" hvor NodeMCU modtager payload beskeder fra
+      client.subscribe(mqtt_topic);
+      // Der kan subscribes til flere specifikke topics
+      //client.subscribe("Test1");
+      // Eller til samtlige topics ved at bruge '#' (Se Power Point fra d. 18. marts)
+      // client.subscribe("#");
+
+      // Hvis forbindelsen fejler køres loopet igen efter 5 sekunder indtil forbindelse er oprettet
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Venter 5 sekunder før den prøver igen
+      delay(5000);
+    }
+  }
+}
+
+///////// OPSÆTNING AF WIFI SLUT /////////
+
+//
+//
+//
+//
+//
+//'
+//
+//
+//
+//
+//
+//
+
+
